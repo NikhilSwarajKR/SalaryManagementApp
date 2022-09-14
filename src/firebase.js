78ddc32f0aa68075d5528273c3ff5a43073cc618
MSCCSMINI <msccsminiproject@gmail.com>
2022-09-14 22:32:22 +0530

    Initial Commit

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCevfjxYh5AOuPpzdQmiqTJOyoDJHZRxRQ",
+  authDomain: "salarymanagementapp-bf7be.firebaseapp.com",
+  projectId: "salarymanagementapp-bf7be",
+  storageBucket: "salarymanagementapp-bf7be.appspot.com",
+  messagingSenderId: "278630086267",
+  appId: "1:278630086267:web:937597dc30e7b764dbd4de"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

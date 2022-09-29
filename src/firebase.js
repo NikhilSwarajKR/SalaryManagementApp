1161f75319bad922a92c46786c778715da8ec385
MSCCSMINI <112877830+MSCCSMINI@users.noreply.github.com>
2022-09-29 19:42:56 +0530

    marina initial commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getFirestore} from '@firebase/firestore'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
+import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCevfjxYh5AOuPpzdQmiqTJOyoDJHZRxRQ",
@@ -14,6 +10,5 @@ const firebaseConfig = {
   appId: "1:278630086267:web:937597dc30e7b764dbd4de"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const db =getFirestore(app);
+  export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);

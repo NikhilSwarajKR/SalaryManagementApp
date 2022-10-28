a5a4099d3483d560c195a23b9ecb6618c1b210c2
MSCCSMINI <msccsminiproject@gmail.com>
2022-10-28 09:52:37 +0530

    Ok

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,8 @@
 import { initializeApp } from "firebase/app";
-<<<<<<< HEAD
 import { getAuth } from "firebase/auth";
-=======
 import {getFirestore} from '@firebase/firestore'
 import {getStorage} from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
->>>>>>> nikhil
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCevfjxYh5AOuPpzdQmiqTJOyoDJHZRxRQ",
   authDomain: "salarymanagementapp-bf7be.firebaseapp.com",
@@ -18,14 +12,9 @@ const firebaseConfig = {
   appId: "1:278630086267:web:937597dc30e7b764dbd4de"
 };
 
-<<<<<<< HEAD
-  export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-=======
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 const db =getFirestore(app);
 const storage = getStorage(app);
 
 export {db,storage};
->>>>>>> nikhil
